073a5d64c85b6392d3a7ab7908a8aa3f069f18c7
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-02-17 17:05:40 -0300

    Update simple message and simple user classes

---
+++ Domain/SimpleBotUser.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBot.Logic
+{
+    public class SimpleBotUser
+    {
+		
+		public string Username { get; internal set; }
+		public string Id { get; internal set; }
+
+		public string Reply(SimpleMessage message)
+        {
+            return $"{message.User} disse '{message.Text}";
+        }
+
+		
+	}
+}
