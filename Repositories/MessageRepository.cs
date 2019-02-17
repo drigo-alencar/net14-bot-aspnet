1de992b8307f12e81697ebeae32fc5b2e0571ad0
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-02-17 17:04:57 -0300

    Create message and user repository

---
+++ Repositories/MessageRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MongoDB.Driver;
+using SimpleBot.Logic;
+
+namespace SimpleBot.Repositories
+{
+	public class MessageRepository
+	{
+		private const string COLLECTION_NAME = "Messages";
+
+		private IMongoCollection<SimpleMessage> collection;
+
+		public MessageRepository(IMongoDatabase db)
+		{
+			this.collection = db.GetCollection<SimpleMessage>(COLLECTION_NAME);
+		}
+
+		internal void Save(SimpleMessage message)
+		{
+			this.collection.InsertOne(message);
+		}
+
+		internal void DeleteUserMessages(string username)
+		{
+			var filter = Builders<SimpleMessage>.Filter.Eq(s => s.User, username);
+			this.collection.DeleteMany(filter);
+		}
+	}
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using SimpleBot.Logic;
+using System;
+using System.Configuration;
+
+namespace SimpleBot.Repositories
+{
+	public class UserRepository
+	{
+		private IMongoCollection<SimpleBotUser> collection;
+
+		public UserRepository(IMongoDatabase db)
+		{
+			this.collection = db.GetCollection<SimpleBotUser>("users");
+		}
+
+		internal SimpleBotUser GetProfile(SimpleBotUser user)
+		{
+			return null;
+		}
+
+		internal void SetProfile(SimpleBotUser user)
+		{
+			
+		}
+	}
+}
