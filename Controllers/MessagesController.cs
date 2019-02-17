fd2414ae50c384e27e30b0bbe2c4b8b7d1a63036
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-02-17 17:06:10 -0300

    Update message controller to save messages and identify delete command

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Configuration;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -7,33 +8,34 @@ using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using MongoDB.Driver;
 using SimpleBot.Logic;
+using SimpleBot.Repositories;
 
 namespace SimpleBot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
-        static SimpleBotUser g_bot = null;
-        private IMongoDatabase db;
+		static SimpleBotUser g_bot = null;
+		private UnitOfWork unitOfWork;
 
-        public MessagesController()
-        {
-            // Pattern: singleton
-            if (g_bot == null)
-            {
-                g_bot = new SimpleBotUser();
-            }
+		public MessagesController()
+		{
+			// Pattern: singleton
+			if (g_bot == null)
+			{
+				g_bot = new SimpleBotUser();
+			}
 
-            db = new MongoClient().GetDatabase("BotBase");
-        }
+			this.unitOfWork = new UnitOfWork();
+		}
+		
 
-        [ResponseType(typeof(void))]
+		[ResponseType(typeof(void))]
         public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
             if ( activity != null && activity.Type == ActivityTypes.Message)
             {
                 await HandleActivityAsync(activity);
-                db.GetCollection<Activity>("activities").InsertOne(activity);
             }
 
             // HTTP 202
@@ -47,7 +49,20 @@ namespace SimpleBot
             string userFromId = activity.From.Id;
             string userFromName = activity.From.Name;
 
+			var user = new SimpleBotUser { Id = userFromId, Username = userFromName };
+			
+
+
             var message = new SimpleMessage(userFromId, userFromName, text);
+
+			if (message.Text.ToLowerInvariant().Contains("delete my messages"))
+			{
+				this.unitOfWork.MessageRepository.DeleteUserMessages(user.Username);
+			}
+			else
+			{
+				this.unitOfWork.MessageRepository.Save(message);
+			}
 
             string response = g_bot.Reply(message);
 
