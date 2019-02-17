e86ca7eb39a0db72f8d808ec0e8e708420c5edd9
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-02-17 17:03:58 -0300

    Create unit of work

---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBot.Repositories
+{
+	public class UnitOfWork
+	{
+		private const string MONGO_CONNECTION_STRING_NAME = "Mongo";
+		private const string MONGO_DATABASE = "BotBase";
+
+		private IMongoDatabase db;
+		private MessageRepository messageRepository;
+
+		public UnitOfWork()
+		{
+			db = new MongoClient(GetConnectionString()).GetDatabase(MONGO_DATABASE);
+		}
+
+		private static string GetConnectionString()
+		{
+			return ConfigurationManager.ConnectionStrings[MONGO_CONNECTION_STRING_NAME].ConnectionString;
+		}
+
+		public MessageRepository MessageRepository
+		{
+			get
+			{
+				if (this.messageRepository == null) { this.messageRepository = new MessageRepository(db); }
+				return this.messageRepository;
+			}
+		}
+	}
+}
